90345d2350fea3777f76959c4a8aee116aef5d92
Flame467v <neilchahil@gmail.com>
2019-10-26 22:54:42 -0400

    Add files via upload
    final

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -27,18 +27,18 @@ public class ProblemSet4 {
 
         // comment out or uncomment as needed
 
-        ps.sum();
-        ps.reverse();
-        ps.digits();
-        ps.average();
-        ps.prime();
-        ps.fibonacci();
-        ps.factors();
-        ps.mario();
-        ps.luigi();
-        ps.credit();
+       ps.sum();
+       ps.reverse();
+       ps.digits();
+       ps.average();
+       ps.prime();
+       ps.fibonacci();
+       ps.factors();
+       ps.mario();
+       ps.luigi();
+       ps.credit();
 
-        in.close();
+       in.close();
     }
 
     /*
@@ -52,7 +52,31 @@ public class ProblemSet4 {
      */
 
     public void sum() {
-int y;
+
+        long sum = 0;
+        long lowerbound = 0;
+        long upperbound = -1;
+
+        System.out.println("");
+
+        do  {
+            System.out.print("Lower bound: ");
+            lowerbound = in.nextLong();
+            System.out.print("Upper bound: ");
+            upperbound = in.nextLong();
+        }
+        while (lowerbound > upperbound);
+
+        if ((lowerbound % 2) != 0) {
+            lowerbound = lowerbound+1;
+        }
+
+        for (long i = lowerbound; i <= upperbound; i = i + 2) {
+            sum += i;
+        }
+
+        System.out.printf("\n%,d.\n\n", sum);
+
     }
 
     /*
@@ -64,6 +88,29 @@ int y;
 
     public void reverse() {
 
+        long integer;
+
+        do  {
+            System.out.print("Positive Integer: ");
+            integer = in.nextLong();
+        }
+        while (integer < 1);
+
+
+        String result = Long.toString(integer);
+        System.out.print("\n");
+
+
+        for (int i = result.length(); i > 0; i--) {
+
+            if (i==1) {
+                System.out.printf("%s.\n\n", result.substring(i-1, i));
+            }
+            else {
+                System.out.printf("%s, ", result.substring(i-1, i));
+            }
+        }
+
     }
 
     /*
@@ -74,6 +121,28 @@ int y;
      */
 
     public void digits() {
+
+        long integer;
+        int sum = 0;
+        int add;
+
+        do {
+            System.out.print("Positive integer: ");
+            integer = in.nextLong();
+        }
+        while (integer < 1);
+
+        String result = Long.toString(integer);
+
+        for (int i = result.length(); i > 0; i--) {
+
+            add = Integer.parseInt(result.substring(i-1, i));
+            if (add % 2 != 0) {
+                sum += add;
+            }
+        }
+
+        System.out.printf("\n%d.\n\n", sum);
 
     }
 
@@ -87,6 +156,25 @@ int y;
 
     public void average() {
 
+        double integer;
+        double total = 0;
+        double count = 0;
+
+        do {
+            System.out.print("Non-negative integer: ");
+            integer = in.nextLong();
+
+            if (integer > 0) {
+                total += integer;
+                count++;
+            }
+
+        }
+        while (integer >= 0);
+
+        double average = total/count;
+        System.out.printf("\n%,.2f.\n\n", average);
+
     }
 
     /*
@@ -96,6 +184,36 @@ int y;
      */
 
     public void prime() {
+
+        boolean prime = true;
+        long integer;
+
+        do {
+            System.out.print("Non-negative integer: ");
+            integer = in.nextLong();
+
+        }
+        while (integer < 0);
+
+        if (integer == 1) {
+            prime = true;
+        }
+        else if (integer == 0) {
+            prime = false;
+        }
+
+        for (int i = 2; i < integer; i++) {
+            if (integer % i == 0 && integer != i) {
+                prime = false;
+            }
+        }
+
+        if (prime) {
+            System.out.println("\nPrime.\n");
+        }
+        else {
+            System.out.println("\nNot prime.\n");
+        }
 
     }
 
@@ -108,6 +226,27 @@ int y;
 
     public void fibonacci() {
 
+        long integer;
+        int prevint = 0;
+        int currentint = 1;
+        int nextint = 1;
+
+        do {
+            System.out.print("Positive integer: ");
+            integer = in.nextLong();
+
+        }
+        while (integer < 1 || integer > 92);
+
+
+        for (int i = 1; i < integer; i++) {
+            nextint = prevint + currentint;
+            prevint = currentint;
+            currentint = nextint;
+        }
+
+        System.out.printf("\n%d.\n\n", nextint);
+
     }
 
     /*
@@ -117,6 +256,28 @@ int y;
      */
 
     public void factors() {
+
+        long integer;
+
+        do
+        {
+            System.out.print("Positive integer: ");
+            integer = in.nextLong();
+
+        }
+        while (integer < 1);
+
+        System.out.printf("\n1, %d", integer);
+
+        for (int i = 2; i <= integer/i; i++) {
+
+            if (integer % i == 0) {
+                System.out.printf(", %d, %d", i, integer/i);
+            }
+
+        }
+
+        System.out.print(".\n\n");
 
     }
 
@@ -129,6 +290,37 @@ int y;
 
     public void mario() {
 
+        long height;
+        long numspaces = 0;
+
+        do
+        {
+            System.out.print("Height: ");
+            height = in.nextLong();
+
+        }
+        while (height < 1 || height > 24);
+
+        System.out.print("\n");
+
+        for (long i = 1; i <= height; i++) {
+
+            for (long j = height-i; j > 0; j--) {
+                System.out.print(" ");
+                numspaces++;
+            }
+
+            for (long h = height+1-numspaces; h > 0; h--) {
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+            numspaces = 0;
+
+        }
+
+        System.out.print("\n");
+
     }
 
     /*
@@ -139,6 +331,46 @@ int y;
      */
 
     public void luigi() {
+
+        long height;
+        long numspaces = 0;
+        long numhashes = 0;
+
+        do
+        {
+            System.out.print("Height: ");
+            height = in.nextLong();
+
+        }
+        while (height < 1 || height > 24);
+
+        System.out.print("\n");
+
+        for (long i = 1; i <= height; i++) {
+
+            for (long j = height-i; j > 0; j--) {
+                System.out.print(" ");
+                numspaces++;
+            }
+
+            for (long h = height+1-numspaces; h > 0; h--) {
+                System.out.print("#");
+                numhashes++;
+            }
+
+            System.out.print("  ");
+
+            for (long k = 0; k < numhashes; k++) {
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+            numspaces = 0;
+            numhashes = 0;
+
+        }
+
+        System.out.print("\n");
 
     }
 
@@ -151,5 +383,27 @@ int y;
 
     public void credit() {
 
+        String visa = "^4[0-9]{12}(?:[0-9]{3})?$";
+        String master = "^5[1-5][0-9]{14}$";
+        String express = "^3[47][0-9]{13}$";
+        String ccn = "null";
+
+   System.out.print("Number: ");
+   ccn = in.next();
+
+        if(ccn.matches(visa)){
+              System.out.print("\nVisa.");
+          }
+        else if (ccn.matches(master)){
+              System.out.print("\nMastercard.");
+          }
+        else if (ccn.matches(express)) {
+              System.out.print("\nAmex.");
+          }
+          else {
+          System.out.print("\nInvalid.");
+        }
+
     }
+
 }
